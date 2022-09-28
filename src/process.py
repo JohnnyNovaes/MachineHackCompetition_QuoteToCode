0ff9542002c65f057d70515d1081b6a106d7bb38
João Henrique Saraceni Novaes <joaosaraceni@JOAOSARACENI.localdomain>
2022-09-27 22:19:13 -0300

    style: Change the names & add sample_submission data

--- src/process.py
+++ src/process.py
@@ -1,42 +1,39 @@
 # files management 
 import hydra
-from hydra import utils
-from omegaconf import DictConfig, OmegaConf
+from hydra import compose, initialize, utils
+from omegaconf import OmegaConf
 import os
-
 import pandas as pd
+import logging
 
-from logzero import logger
-
-@hydra.main(config_path="../config", config_name="main")
-def load_data(config: DictConfig) -> None:
+class DataGetter():
     """Load data and build dataframes."""
-
-    # Build dataframes
-    logger.info("Loading datasets")
     
-    current_path = utils.get_original_cwd()
+    def __init__(self):
+        # Build dataframes
+        logging.info("Loading datasets")
+        
+        initialize(version_base=None, config_path="../config", job_name="DataGetter")
+        cfg = compose(config_name="data_getter")
+        
+        current_path = os.getcwd()
+        
+        actual_set_path = os.path.join(current_path, cfg.raw_path.actual_set)
+        self.actual_set_df = pd.read_csv(actual_set_path)
 
-    actual_set_path = os.path.join(current_path, config.raw_path.actual_set_path)
-    actual_set_df = pd.read_csv(actual_set_path)
+        app_installs_path = os.path.join(current_path, cfg.raw_path.app_installs)
+        self.app_installs_df = pd.read_csv(app_installs_path)
 
-    app_installs_path = os.path.join(current_path, config.raw_path.app_installs_path)
-    app_installs_df = pd.read_csv(app_installs_path)
+        app_metadata_path = os.path.join(current_path, cfg.raw_path.app_metadata)
+        self.app_metadata_df = pd.read_csv(app_metadata_path)
 
-    app_metadata_path = os.path.join(current_path, config.raw_path.app_metadata_path)
-    app_metadata_df = pd.read_csv(app_metadata_path)
+        app_usage_path = os.path.join(current_path, cfg.raw_path.app_usage)
+        self.app_usage_df = pd.read_csv(app_usage_path)
 
-    app_usage_path = os.path.join(current_path, config.raw_path.app_usage_path)
-    app_usage_df = pd.read_csv(app_usage_path)
+        user_metadata_path = os.path.join(current_path, cfg.raw_path.user_metadata)
+        self.user_metadata_df = pd.read_csv(user_metadata_path)
 
-    user_metadata_path = os.path.join(current_path, config.raw_path.user_metadata_path)
-    user_metadata_df = pd.read_csv(user_metadata_path)
+        sample_submission_path = os.path.join(current_path, cfg.raw_path.sample_submission)
+        self.sample_submission_df = pd.read_csv(sample_submission_path)
 
-    logger.info("Loading datasets finished successfully!")
-    
-def join_data() -> None:
-    """Join dataframes by User ID."""
-    pass
-
-if __name__ == "__main__":
-    load_data() 
+        logging.info("Loading datasets finished successfully!")
